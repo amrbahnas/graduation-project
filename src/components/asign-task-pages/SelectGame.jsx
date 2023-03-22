44bc044e64196a4e6cb3ccf70cceae3b93f6e357
Amr Elbahnsawy <73616484+amrbahnas@users.noreply.github.com>
2023-03-22 21:14:30 +0200

    games images

--- src/components/asign-task-pages/SelectGame.jsx
+++ src/components/asign-task-pages/SelectGame.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Common.css";
 import { motion } from "framer-motion";
 const SelectGame = () => {
-  const Cart = ({ title, img }) => {
+  const Cart = ({ game }) => {
     return (
       <motion.div
         className="cart"
@@ -10,8 +10,8 @@ const SelectGame = () => {
         animate={{ scale: 1 }}
         onClick={selectGame}
       >
-        <img src="../../../public/assets/images/arabic.jpg" alt="" />
-        <span className="title">{title}</span>
+        <img src={`../../../public/assets/images/games/${game}.jpeg`} alt="" />
+        <span className="title">{game}</span>
       </motion.div>
     );
   };
@@ -23,12 +23,12 @@ const SelectGame = () => {
   };
   return (
     <div className="select-game" id="game">
-      <Cart title={"game1"} />
-      <Cart title={"game1"} />
-      <Cart title={"game1"} />
-      <Cart title={"game1"} />
-      <Cart title={"game1"} />
-      <Cart title={"game1"} />
+      <Cart game={"game1"} />
+      <Cart game={"game2"} />
+      <Cart game={"game3"} />
+      <Cart game={"game4"} />
+      <Cart game={"game5"} />
+      <Cart game={"game6"} />
     </div>
   );
 };
