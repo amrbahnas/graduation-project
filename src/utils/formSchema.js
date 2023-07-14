21371807dbd2d436772982e052cc85f84a5d4806
Amr Elbahnsawy <eng.elbahnsawy@gmail.com>
2023-07-14 22:34:49 +0300

    add email mail validation

--- src/utils/formSchema.js
+++ src/utils/formSchema.js
@@ -4,7 +4,14 @@ export const SignupSchema = Yup.object().shape({
   name: Yup.string()
     .min(3, "Name must be at least 3 characters")
     .required("Required"),
-  email: Yup.string().email("Invalid email address").required("Required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .test("is-gmail", "Only Gmail addresses are allowed", (value) => {
+      // Regular expression to validate Gmail email address
+      const regex = /^[a-zA-Z0-9_.+-]+@gmail.com$/;
+      return regex.test(value);
+    })
+    .required("Required"),
   password: Yup.string()
     .min(8, "Password must be at least 8 characters")
     .required("Required"),
