3ad8b8cb2b1f5f582067329f305ac43005dfe79a
Amr Elbahnsawy <eng.elbahnsawy@gmail.com>
2023-07-15 01:16:08 +0300

    hot fix

--- src/hooks/useFeedback.js
+++ src/hooks/useFeedback.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import ApiClient from "../services/api-client";
+import { useQuery } from "@tanstack/react-query";
 
 const useFeedback = (taskID) => {
   const { _id } = useParams();
